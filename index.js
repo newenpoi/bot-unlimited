cc90e26a16a3e500c4e1716b5e93f133d56f3e4b
newenpoi <lammenschristopher119@hotmail.com>
2022-05-27 01:19:16 +0200

    wrap

--- index.js
+++ index.js
@@ -5,32 +5,39 @@ const path = require('node:path');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
+function loadCommands() {
+    // New integrated collection of command definitions.
+    client.commands = new Collection();
+    const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        // Gets the content of command file.
+        const command = require(`./commands/${file}`);
+
+        // Key as command name and exported module as value.
+        client.commands.set(command.data.name, command);
+    }
+}
+
+function loadEvents() {
+    // Gets events files.
+    const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+        // Gets event file content.
+        const event = require(`./events/${file}`);
+        
+        // Calls the execute function depending on the event.
+        if (event.once) client.once(event.name, (...args) => event.execute(...args));
+        else client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
 // A new client instance...
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// New integrated collection of command definitions.
-client.commands = new Collection();
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    // Gets the content of command file.
-    const command = require(`./commands/${file}`);
-
-    // Key as command name and exported module as value.
-    client.commands.set(command.data.name, command);
-}
-
-// Gets events files.
-const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    // Gets event file content.
-	const event = require(`./events/${file}`);
-	
-    // Calls the execute function depending on the event.
-    if (event.once) client.once(event.name, (...args) => event.execute(...args));
-    else client.on(event.name, (...args) => event.execute(...args));
-}
+loadCommands();
+loadEvents();
 
 // Connects to discord...
 client.login(token);
