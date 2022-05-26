34b12756829acca050ddb7f6d7258d8ff1aa433c
newenpoi <lammenschristopher119@hotmail.com>
2022-05-27 01:20:32 +0200

    syncs user model

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,12 @@
+const user = require('../models/User.js');
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`Our client is ready for integration and logged in as ${client.user.tag}.`);
+
+		// Synchronizes the User model.
+		user.sync();
 	},
 };
