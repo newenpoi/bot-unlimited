ced510620899014b6c57d969d081a0fd0e38bf32
newenpoi <lammenschristopher119@hotmail.com>
2022-05-27 16:55:32 +0200

    custom string formatting

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,20 @@
+/*
+	Author : Newen
+
+	The ping command.
+*/
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const reader = require('../util/reader.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Renvoie la latence de cette application.'),
 	async execute(interaction) {
-		await interaction.reply(`Latence actuelle... ${Math.abs(Date.now() - interaction.createdTimestamp) / 100} ms.`);
+		// Gets the response required for this interaction.
+		let line = await reader.read(interaction.commandName, 'latency', (Math.abs(Date.now() - interaction.createdTimestamp)));
+		
+		await interaction.reply(line);
 	},
 };
--- util/reader.js
+++ util/reader.js
@@ -1,17 +1,40 @@
+/*
+	Author: Newen
+	Contributors :
+	
+	This file is used to handle ultra customized responses through html files,
+	Will serve for multi-languages features.
+*/
+
 const utf8 = require('utf8');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
+
+String.prototype.format = function() {
+	var args = arguments;
+
+	// Using replace to iterate over the string.
+	// Select the match and check if the related argument is present, and replaces the match with the argument.
+	return this.replace(/{([0-9]+)}/g, function (match, index) {
+		// Check if the argument is present.
+		return typeof args[index] == 'undefined' ? match : args[index];
+	});
+},
 
 module.exports = {
 	name: 'reader',
 	/**
 	 *
-	 * @param {Le fichier dont on va chercher la réponse.} file
-	 * @param {La balise identifiée dont on récupère le contenu.} identifier
+	 * @param {string} file - The file in which we'll search for the attribute.
+	 * @param {string} identifier - The id attribute from this file to get the response.
+	 * @param {string=} args - The rest of the arguments included in the response.
 	 */
-	async read(file, identifier) {
-        // Parses the file to retrieve element and returns it's innerHTML (we need the decoding).
+	async read(file, identifier, ...args) {
+        // Parses the file (dom) in order to access custom data through an id.
 		const dom = await JSDOM.fromFile(`strings/${file}.html`);
-		return utf8.decode(dom.window.document.querySelector(`#${identifier}`).innerHTML);
+		const content = dom.window.document.querySelector(`#${identifier}`).innerHTML;
+
+		// Sends back the formatted string with the rest.
+		return utf8.decode(content.format(args));
 	},
 };
